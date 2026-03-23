c4481df780438749e08a69499b54a3dbbc64f072
swethabhimari <swethabhimari@gmail.com>
2026-03-23 21:54:21 +0530

    Add files via upload

---
+++ typ_Cast.py
@@ -0,0 +1,62 @@
+#Convert int into float
+print("1.convert int into float")
+num=10
+num_float=float(num)
+print("int to float",num_float)
+
+#Convert float into int
+print("2.Convert float into int")
+f=12.7
+n_int=int(f)
+print("float to int",n_int)
+
+
+#take strint 123 to int
+print("3.strint to int")
+s="123"
+s_int=int(s)
+print(s_int)
+
+#convert int to string and print with text
+print(4.)
+a=10
+a_str=str(a)
+print("number is:",a_str)
+
+#take num as ip and convert to int
+print(5.)
+num_input=input("Enter any num:")
+num_int=int(num_input)
+print("enterger is:",num_int)
+
+
+#convert float to string and print
+print(6.)
+f=3.0
+f_str=str(f)
+print("float to string:",f_str)
+
+#add 2 nums as strings and convert into int
+print(7.)
+n="20"
+m="30"
+sum=int(n)+int(m)
+print("sum:",sum)
+
+
+#convert "45.6" to float
+print(8.)
+f="45.6"
+f_float=float(f)
+print("string to float:",f_float)
+
+#convert num into str and concatinate(add)
+print(9.)
+num1=100
+word="this is :"+str(num1)
+print("word:",word)
+
+#display type of converted var
+print(10.)
+var=float("50")
+print("value:",var,"type:",type(var))
