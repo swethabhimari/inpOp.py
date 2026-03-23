4d4d2aef3b7664feae4581cf2286be398caf0424
swethabhimari <swethabhimari@gmail.com>
2026-03-23 21:28:52 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,62 @@
+#store var and print
+print("1.create var and print it")
+name="Swetha"
+print( "My name is:",name)
+
+#store var and print sum
+print("2.store 2 variable and print sum:")
+n1=2
+n2=3
+sum=n1+n2
+print("sum=",sum)
+
+#swap two values
+print("3.Swap 2 numbers:")
+a=5
+b=6
+a,b=b,a
+print("After swapping:a=",a ,"b=",b)
+
+#calculate area of a rectangle
+print("4.Area of rectangle")
+length=8
+width=6
+area= length*width
+print("Area of a rectangle:",area)
+
+#store age in var and print
+print("5,Store age in var and print")
+age=16
+print("My age is:",age)
+
+#create 3 var and print them in one line
+print("6.Create 3 vars and print in one line")
+x=10
+y=20
+z=30
+print(x,y,z)
+
+#store price in var and add tax to it
+print("7.Store price in var add tax")
+price=100
+tax=18
+total_cost=price+tax
+print("after addind tax:",total_cost)
+
+#Assign multipe values to multiple var in one line
+print("8.Assign multipe values to multiple var in one line")
+p,q,r,s=10,20,30,40
+print(p,q,r)
+
+#create var for first and last and combine both
+print("9")
+first_name="Swetha"
+last_name="Gnaneshwari"
+full_name=first_name+last_name
+print("fullname",full_name)
+
+#Store a num and print its cube
+print("10.Store a num and print its cube")
+num=4
+cube=num**3
+print("CUBE of",num, "is" ,cube)
